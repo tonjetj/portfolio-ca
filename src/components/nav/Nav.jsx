c597726d322e90bb32cef1da2827ed5079dea17d
tonjetj <tontotjen@gmail.com>
2023-06-11 22:43:46 +0200

    changed url

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,19 +12,19 @@ const Nav = () => {
     <div className='container nav__container'>
       <h3>Tonje T. Jenssen</h3>
       <nav>
-        <HashLink smooth to="/#" onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active nav-link' : 'nav-link'} id='home'>
+        <HashLink smooth to="/portfolio-ca/#" onClick={() => setActiveNav('/')} className={activeNav === '/' ? 'active nav-link' : 'nav-link'} id='home'>
           <RiHome5Line alt='House Emoji'/>
           <p>Home</p>
         </HashLink>
-        <HashLink smooth to="/#about" onClick={() => setActiveNav('/about')} className={activeNav === '/about' ? 'active nav-link' : 'nav-link'}>
+        <HashLink smooth to="/portfolio-ca/#about" onClick={() => setActiveNav('/about')} className={activeNav === '/about' ? 'active nav-link' : 'nav-link'}>
           <BsEmojiSmile alt='Smile Emoji'/>
           <p>About Me</p>
         </HashLink>
-        <HashLink smooth to="/#work" onClick={() => setActiveNav('/work')} className={activeNav === '/work' ? 'active nav-link' : 'nav-link'}>
+        <HashLink smooth to="/portfolio-ca/#work" onClick={() => setActiveNav('/work')} className={activeNav === '/work' ? 'active nav-link' : 'nav-link'}>
           <GiSpellBook alt='Spellbook Emoji'/>
           <p>Work</p>
         </HashLink>
-        <HashLink smooth to="/#contact" onClick={() => setActiveNav('/contact')} className={activeNav === '/contact' ? 'active nav-link' : 'nav-link'}>
+        <HashLink smooth to="/portfolio-ca/#contact" onClick={() => setActiveNav('/contact')} className={activeNav === '/contact' ? 'active nav-link' : 'nav-link'}>
           <RiChatSmile3Line alt='Chat Boble Emoji'/>
           <p>Contact</p>
         </HashLink>
